473329b906478dd120bc8085afc6f8c83b715e91
AWS ParallelCluster user <ec2-user@ip-10-12-70-55.us-west-2.compute.internal>
2023-12-26 22:18:42 +0000

    auto commit from tony makefile nov2023

--- 02_xfer_files/scp_to_tallgrass.sh
+++ 02_xfer_files/scp_to_tallgrass.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-start_y='1985'
-end_y='1985'
+start_y='1986'
+end_y='1988'
 
 for ((year=start_y; year<=end_y; year++)); do {
 	dest="butzer@tallgrass.cr.usgs.gov:/caldera/projects/usgs/water/impd/butzer/timesync/${year}/"
