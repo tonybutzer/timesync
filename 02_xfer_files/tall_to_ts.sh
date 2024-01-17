ef2d92788409b1a6f1c025b2eac07d2f09648a21
Robison <crobison@igskmncnlxvs350.gs.doi.net>
2024-01-17 08:22:06 -0600

    auto commit from tony makefile nov2023

--- 02_xfer_files/tall_to_ts.sh
+++ 02_xfer_files/tall_to_ts.sh
@@ -1,18 +1,14 @@
 #! /bin/bash
 
-start_y='2000'
-end_y='2000'
+start_y='2001'
+end_y='2001'
 
 for ((year=start_y; year<=end_y; year++)); do {
 	dest="/data/lcmap/www/storages/prj_202301"
 
-	srccsv="butzer@tallgrass.cr.usgs.gov:/caldera/projects/usgs/water/impd/butzer/timesync/${year}"
+	# srccsv="butzer@tallgrass.cr.usgs.gov:/caldera/projects/usgs/water/impd/butzer/timesync/${year}"
+	srccsv="butzer@tallgrass.cr.usgs.gov:timesync/${year}"
 
-	# run it twice to catch any simple errors
-	for sub in b743 b432 ; do {
-		#rsync -azv $srccsv $dest
-		mkdir -p ${dest}/${year}/prj_1211/
-		rsync -azv ${srccsv}/prj_1211/${sub} ${dest}/${year}/prj_1211/ &
-	} done
+	rsync -azv ${srccsv} ${dest}
 } done
 
