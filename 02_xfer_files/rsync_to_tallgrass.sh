d7e0a7e9ca16067500d97185f411240f97baf07e
AWS ParallelCluster user <ec2-user@ip-10-12-70-55.us-west-2.compute.internal>
2024-01-17 18:14:55 +0000

    auto commit from tony makefile nov2023

--- 02_xfer_files/rsync_to_tallgrass.sh
+++ 02_xfer_files/rsync_to_tallgrass.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
-start_y='2000'
-end_y='2005'
+start_y='2003'
+end_y='2003'
 
 for ((year=start_y; year<=end_y; year++)); do {
-	dest="butzer@tallgrass.cr.usgs.gov:/caldera/projects/usgs/water/impd/butzer/timesync/${year}/"
+	#dest="butzer@tallgrass.cr.usgs.gov:/caldera/projects/usgs/water/impd/butzer/timesync/${year}/"
+	dest="butzer@tallgrass.cr.usgs.gov:.//timesync/${year}/"
 
 	srccsv="/efs/timesync/${year}/"
 
 	# run it twice to catch any simple errors
 	rsync -azv $srccsv $dest
-	rsync -azv $srccsv $dest
+	#rsync -azv $srccsv $dest
 } done
 
 
---
+++ 02_xfer_files/tc_b432_b743_to_tall_home.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+start_y='2003'
+end_y='2003'
+
+for ((year=start_y; year<=end_y; year++)); do {
+	#dest="butzer@tallgrass.cr.usgs.gov:/caldera/projects/usgs/water/impd/butzer/timesync/${year}/"
+	home='./timesync/'
+	dest="butzer@tallgrass.cr.usgs.gov:./timesync/"
+
+	srccsv="/efs/timesync/${year}/"
+
+	# run it twice to catch any simple errors
+	#rsync -azv $srccsv $dest &
+	#rsync -azv $srccsv $dest
+	
+	 for sub in b743 b432 tc; do {
+                #rsync -azv $srccsv $dest
+                ssh butzer@tallgrass.cr.usgs.gov mkdir -p ${home}/${year}/prj_1211/
+                rsync -azv ${srccsv}/prj_1211/${sub} ${dest}/${year}/prj_1211/ &
+        } done
+
+} done
+
+
+
