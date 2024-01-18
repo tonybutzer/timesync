d462ac63d327d8f7d65fe7744a9e91d0c3ee3fef
AWS ParallelCluster user <ec2-user@ip-10-12-71-200.us-west-2.compute.internal>
2024-01-18 15:56:24 +0000

    auto commit from tony makefile nov2023

--- 02_xfer_files/2_charles/rsync_to_tallgrass.sh
+++ 02_xfer_files/2_charles/rsync_to_tallgrass.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
-start_y='2006'
-end_y='2006'
+start_y='2023'
+end_y='2023'
 
 for ((year=start_y; year<=end_y; year++)); do {
-	dest="butzer@tallgrass.cr.usgs.gov:/caldera/projects/usgs/eros/usmart/timesync/${year}/"
+	dest="crobison@tallgrass.cr.usgs.gov:/caldera/projects/usgs/eros/users/crobison/timesync/${year}/"
+	#dest="butzer@tallgrass.cr.usgs.gov:/caldera/projects/usgs/eros/usmart/timesync/${year}/"
 	#dest="butzer@tallgrass.cr.usgs.gov:/caldera/projects/usgs/water/impd/butzer/timesync/${year}/"
 	#dest="butzer@tallgrass.cr.usgs.gov:./timesync/${year}/"
 
